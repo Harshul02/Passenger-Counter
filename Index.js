6fbab1f1c910c135f6cd4273152e5e59079e528c
Harshul Jain <harshulgangwal02@gmail.com>
2022-05-03 12:27:10 +0530

    Added Buttons

---
+++ Index.js
@@ -0,0 +1,10 @@
+// document.getElementById("count-el").innerHTML=5;
+// let count=0;
+// count=3;
+// console.log(count);
+let count=0;
+function increment()
+{
+    count++;
+    document.getElementById("count-el").innerHTML=count;
+}
